1fb41eb6e9ceb4d3634d25bf488444baed80137c
seeron6 <49841737+seeron6@users.noreply.github.com>
2026-01-03 16:12:30 -0500

    Fix blank page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This ensures assets are loaded relatively, fixing the blank screen on GitHub Pages
   define: {
     // This prevents "Uncaught ReferenceError: process is not defined"
     'process.env': {}
